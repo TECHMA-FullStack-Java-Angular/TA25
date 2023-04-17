be0e05e6409cde587cccba38971298cf45e27dfe
Paula Sousa <paulasousa137@gmail.com>
2023-04-17 11:34:11 +0200

    dto articulo y fabricante

---
+++ Ejercicio1/src/main/java/com/example/demo/dto/Articulo.java
@@ -0,0 +1,97 @@
+package com.example.demo.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="articulos")
+public class Articulo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	private String nombre;
+	private int precio;
+	@ManyToOne
+	@JoinColumn(name="codigo")
+	private int fabricante;//Cada articulo viene de 1 fabricante
+	
+	//Contrusctores
+	
+	public Articulo(int codigo, String nombre, int precio, int fabricante) {
+		
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.fabricante = fabricante;
+	}
+	
+	
+
+	public Articulo() {
+		
+	}
+
+
+	//Getters & Setters
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getPrecio() {
+		return precio;
+	}
+
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+
+	public int getFabricante() {
+		return fabricante;
+	}
+
+
+	public void setFabricante(int fabricante) {
+		this.fabricante = fabricante;
+	}
+
+
+	
+	//ToString
+	@Override
+	public String toString() {
+		return "Articulo [codigo=" + codigo + ", nombre=" + nombre + ", precio=" + precio + ", fabricante=" + fabricante
+				+ "]";
+	}
+	
+
+	
+	
+	
+
+	
+	
+}
---
+++ Ejercicio1/src/main/java/com/example/demo/dto/Fabricante.java
@@ -0,0 +1,83 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="fabricantes")
+public class Fabricante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	private String nombre;
+	@OneToMany
+	@JoinColumn(name="codigo")
+	private List<Articulo> articulo;//un fabricante puede tener muchos articulos
+	
+	//Constructores
+	
+	public Fabricante(int codigo, String nombre, List<Articulo> articulo) {
+		
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.articulo = articulo;
+	}
+	
+	
+
+	public Fabricante() {
+		
+	}
+
+
+
+	//Getters & Setters
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public List<Articulo> getArticulo() {
+		return articulo;
+	}
+
+
+	public void setArticulo(List<Articulo> articulo) {
+		this.articulo = articulo;
+	}
+
+
+	
+	//To String
+	@Override
+	public String toString() {
+		return "Fabricante [codigo=" + codigo + ", nombre=" + nombre + ", articulo=" + articulo + "]";
+	}
+	
+	
+
+}
