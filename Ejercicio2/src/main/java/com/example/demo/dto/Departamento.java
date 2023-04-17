717db0af711a9167e3a27babeabf094f714c9988
Paula Sousa <paulasousa137@gmail.com>
2023-04-17 18:55:55 +0200

    Ej2 dto

---
+++ Ejercicio2/src/main/java/com/example/demo/dto/Departamento.java
@@ -0,0 +1,87 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="departamentos")
+public class Departamento {
+	
+	@Column(name="codigo")
+	@Id
+	private int id;
+	private String nombre;
+	private int presupuesto;
+
+	@OneToMany
+    @JoinColumn(name="departamento")
+    private List<Empleado> empleado;
+	
+	
+
+	//Contructores
+	public Departamento(int id, String nombre, int presupuesto, List<Empleado> empleado) {
+	
+		this.id = id;
+		this.nombre = nombre;
+		this.presupuesto = presupuesto;
+		this.empleado = empleado;
+	}
+
+	public Departamento() {
+		
+	}
+	
+	//Getters & Setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPresupuesto() {
+		return presupuesto;
+	}
+
+	public void setPresupuesto(int presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+
+	public List<Empleado> getEmpleado() {
+		return empleado;
+	}
+
+	public void setEmpleado(List<Empleado> empleado) {
+		this.empleado = empleado;
+	}
+
+	//ToString
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nombre=" + nombre + ", presupuesto=" + presupuesto + ", empleado="
+				+ empleado + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ Ejercicio2/src/main/java/com/example/demo/dto/Empleado.java
@@ -0,0 +1,80 @@
+package com.example.demo.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name="empleados")
+public class Empleado {
+	@Id
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	
+	@ManyToOne
+	@JoinColumn(name="departamento")
+	private Departamento departamento;
+	
+	
+
+	//Constructores
+	public Empleado(String dni, String nombre, String apellidos, Departamento departamento) {
+		
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.departamento = departamento;
+	}
+
+	public Empleado() {
+		
+	}
+	
+	//Getters & Setters
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	//ToString
+	@Override
+	public String toString() {
+		return "Empleado [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", departamento="
+				+ departamento + "]";
+	}
+	
+	
+
+	
+}
